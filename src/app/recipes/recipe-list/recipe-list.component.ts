1645c821c652f8b92ce4bb42f6b8f0567017bfdb
Minh Bui <tranduyminh.bui@gmail.com>
2020-09-17 19:00:10 +1000

    finished Recipe component html layout

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Test Recipe",
+      "Description test",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/szechuan-beef-horizontal-1530892577.jpg?resize=768:*"
+    ),
+    new Recipe(
+      "Test Recipe 2",
+      "Description 2 test",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/szechuan-beef-horizontal-1530892577.jpg?resize=768:*"
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
