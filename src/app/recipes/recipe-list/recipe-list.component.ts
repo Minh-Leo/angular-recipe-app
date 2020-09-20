1f4503c778d342dbb1df51264b085cdaff631d1d
Minh Bui <tranduyminh.bui@gmail.com>
2020-09-20 22:18:19 +1000

    passed recipe selected from recipe-item to recipe-detail the super long way just for fun

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 
 import { Recipe } from "../recipe.model";
 
@@ -20,8 +20,13 @@ export class RecipeListComponent implements OnInit {
       "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/szechuan-beef-horizontal-1530892577.jpg?resize=768:*"
     ),
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
