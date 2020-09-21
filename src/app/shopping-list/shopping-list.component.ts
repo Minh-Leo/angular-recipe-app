83ed54b4c6dc6fba2a9b31926efec2638c875b5f
Minh Bui <tranduyminh.bui@gmail.com>
2020-09-21 19:29:39 +1000

    added dropdown directive for dropdown menu

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -16,4 +16,8 @@ export class ShoppingListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onIngredientAdded(newIngredient: Ingredient) {
+    this.ingredients.push(newIngredient);
+  }
 }
