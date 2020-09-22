e4902bc41947428d63c5ecf1e81ce45598cde0e4
Minh Bui <tranduyminh.bui@gmail.com>
2020-09-22 20:14:31 +1000

    transfer shoppingList component from property binding approach to service approach, addIngredientsToShoppingList button work

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -8,17 +11,25 @@ export class RecipeService {
     new Recipe(
       "Test Recipe",
       "Description test",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/szechuan-beef-horizontal-1530892577.jpg?resize=768:*"
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/szechuan-beef-horizontal-1530892577.jpg?resize=768:*",
+      [new Ingredient("Meat", 1), new Ingredient("Rice", 20)]
     ),
     new Recipe(
       "Test Recipe 2",
       "Description 2 test",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/szechuan-beef-horizontal-1530892577.jpg?resize=768:*"
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/szechuan-beef-horizontal-1530892577.jpg?resize=768:*",
+      [new Ingredient("Chicken", 31), new Ingredient("Bun", 8)]
     ),
   ];
+
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     // return a copy of the array, without slice it return a reference
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
