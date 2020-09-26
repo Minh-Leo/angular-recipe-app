f5851a327ad64d9a1b0c5628636f62241bc486a2
Minh Bui <tranduyminh.bui@gmail.com>
2020-09-26 18:56:26 +1000

    implemented form template and edit item works

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,8 @@ import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
+
   private ingredients: Ingredient[] = [
     new Ingredient("Apple", 5),
     new Ingredient("Beef", 9),
@@ -11,6 +13,9 @@ export class ShoppingListService {
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+  getIngredient(index) {
+    return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient) {
@@ -21,6 +26,11 @@ export class ShoppingListService {
   addIngredients(ingredients: Ingredient[]) {
     // ... spread the array into a list of ingredients to push
     this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
